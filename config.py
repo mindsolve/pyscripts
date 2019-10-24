41b575c835b03383eca9b47f1a00eca67bbf5c57
Felix Ern <felix.ern@vodafone.com>
2019-10-24 12:55:42 +0200

    Caching decrypted data for faster startup

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-# This configuration file contains all confidential information
+"""
+This configuration file contains all confidential information
+"""
 from simplecrypt import decrypt, DecryptionException
 import sys
 import os
@@ -9,12 +11,18 @@ import base64
 if __name__ == '__main__':
     print('This script is not meant to be called directly.')
     sys.exit(1)
+
+if os.path.isfile(os.path.join(os.path.dirname(__file__), 'config_decrypted.json')):
+    print("Loading configuration from decrypted cache...")
+    config_json = open(os.path.join(os.path.dirname(__file__), 'config_decrypted.json'), mode="r").read()
+
 else:
     print("Loading encrypted configuration... ", end="")
     if 'KEY' in os.environ:
-        print('Using password from ENV. ')
+        print('Using password from ENV.')
         password = os.getenv('KEY')
     else:
+        print('Using interactive input.')
         try:
             password = input("\nPlease enter the password: ")
         except EOFError:
@@ -40,17 +48,22 @@ else:
         print('ERROR:', e)
         sys.exit(1)
 
-    config_obj = json.loads(config_json)
+    # Caching decrypted json text to file, as decryption is quite slow (>5 seconds)
+    # This encryption is purely to protect internal data (COMPANY INTERNAL), not for actual "confidentiality"/"secrecy"
+    open(os.path.join(os.path.dirname(__file__), 'config_decrypted.json'), mode="w").write(config_json)
 
-    # End of decryption, now assign config variables
 
-    proxy_dict = config_obj["proxy_dict"]
-    baseurl = config_obj["baseurl"]
+config_obj = json.loads(config_json)
 
-    # Newton username
-    newton_username = config_obj['newton_username']
-    newton_password = config_obj['newton_password']
+# End of decryption, now assign config variables
 
-    basedn = config_obj['basedn']
-    ldap_url = config_obj['ldap_url']
-    user_basedn = config_obj['user_basedn']
+proxy_dict = config_obj["proxy_dict"]
+baseurl = config_obj["baseurl"]
+
+# Newton username
+newton_username = config_obj['newton_username']
+newton_password = config_obj['newton_password']
+
+basedn = config_obj['basedn']
+ldap_url = config_obj['ldap_url']
+user_basedn = config_obj['user_basedn']
